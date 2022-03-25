a6f9124391f01e3aa22862bd2fcfcb92f847a88a
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-03-24 23:23:43 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult game_setup()
+        {
+
+            return View("game_setup");
+        }
+
+        public ActionResult About()
+        {
+	        return View("About");
+        }
+
+        public ActionResult Game_Rules()
+        {
+	        return View("game_rules");
+        }
+
+        public ActionResult Game()
+        {
+	        return View("game");
+        }
+    }
+}
